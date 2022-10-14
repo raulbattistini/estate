5f08a962409552f194507b5bec95bc78485f506d
Raul Battistini <raulbattistini.3@gmail.com>
2022-10-14 06:27:19 -0300

    upd: first sketch for homepage

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import { Footer } from '../../components/Footer'
-import { Header } from '../../components/Header'
+import { Grid } from "@mui/material";
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { AiOutlineSearch } from "react-icons/ai";
+import CityIcon from "../../assets/images/buildings-icon.png";
 
 export const Home = () => {
   return (
     <>
-    <Header/>
-    Home
-    <Footer/>
+      <Header />
+      <div className="bg-[#21a0a0] w-full">
+        <div className="flex self-center justify-center pt-3">
+          <input
+            type="text"
+            className="p-2 rounded-md pr-12 w-96 outline-0"
+            placeholder="What you are looking for?"
+          />
+          <AiOutlineSearch className="absolute text-3xl ml-44 mt-1 hover:text-[#a18276]" />
+        </div>
+        <div className="self-center flex justify-center">
+          <img src={CityIcon} alt="buildings icon"  className="static"/>
+        </div>
+        <span className="text-white">Home </span>
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
