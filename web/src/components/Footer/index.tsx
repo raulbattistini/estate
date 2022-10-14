5f85cbeab8d7c05b789d5ee9cfdde82fdebc5362
Raul Battistini <raulbattistini.3@gmail.com>
2022-10-14 16:08:21 -0300

    rm: useless line of code

--- web/src/components/Footer/index.tsx
+++ web/src/components/Footer/index.tsx
@@ -28,13 +28,14 @@ export const Footer = () => {
           Where we are located{" "}
         </span>
         <div className="flex justify-center rounded-md pt-3">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224444.54325527028!2d-81.34266519999998!3d28.4811732!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e773d8fecdbc77%3A0xac3b2063ca5bf9e!2sOrlando%2C%20FL%2C%20USA!5e0!3m2!1sen!2sbr!4v1665736536646!5m2!1sen!2sbr"
-          width="600"
-          height="450"
-          loading="lazy"
-          className="rounded-md"
-        ></iframe></div>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224444.54325527028!2d-81.34266519999998!3d28.4811732!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e773d8fecdbc77%3A0xac3b2063ca5bf9e!2sOrlando%2C%20FL%2C%20USA!5e0!3m2!1sen!2sbr!4v1665736536646!5m2!1sen!2sbr"
+            width="600"
+            height="450"
+            loading="lazy"
+            className="rounded-md"
+          ></iframe>
+        </div>
       </div>
       <span className="text-white text-3xl text-center flex justify-center p-5">
         {" "}
@@ -62,14 +63,26 @@ export const Footer = () => {
           </div>
         </Grid>
       </Grid>
-    <span> 
-      <Grid container className="text-white text-center pb-4 text-xs">
-      <Grid item xs={3} className="hover:text-[#c8c8c8] cursor-pointer"> <div className="flex text-center justify-center"> </div> Terms of Service  </Grid>
-      <Grid item xs={3} className="hover:text-[#c8c8c8] cursor-pointer"> Privacy Policy </Grid>
-      <Grid item xs={3} className="hover:text-[#c8c8c8] cursor-pointer"> Oficial Realtors of Florida </Grid> 
-      <Grid item xs={3} className="hover:text-[#c8c8c8] cursor-pointer"> DMCA Notice </Grid>  
-      </Grid>
-    </span>
+      <span>
+        <Grid container className="text-white text-center pb-4 text-xs">
+          <Grid item xs={3} className="hover:text-[#c8c8c8] cursor-pointer">
+            {" "}
+            Terms of Service{" "}
+          </Grid>
+          <Grid item xs={3} className="hover:text-[#c8c8c8] cursor-pointer">
+            {" "}
+            Privacy Policy{" "}
+          </Grid>
+          <Grid item xs={3} className="hover:text-[#c8c8c8] cursor-pointer">
+            {" "}
+            Oficial Realtors of Florida{" "}
+          </Grid>
+          <Grid item xs={3} className="hover:text-[#c8c8c8] cursor-pointer">
+            {" "}
+            DMCA Notice{" "}
+          </Grid>
+        </Grid>
+      </span>
     </div>
   );
 };
