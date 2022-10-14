6e0f757115394060a62c9aba6c1c8edd0a131a55
Raul Battistini <raulbattistini.3@gmail.com>
2022-10-14 16:07:30 -0300

    fix: host db missing

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -7,6 +7,7 @@ export const connection = new DataSource({
     database: process.env.DB_NAME,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT as unknown as number,
     entities: [User],
     migrations: ['./migrations/']
