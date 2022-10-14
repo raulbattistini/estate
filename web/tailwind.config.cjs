92c460a84be6ec6ad36012f21ea7a5957487154f
Raul Battistini <raulbattistini.3@gmail.com>
2022-10-14 06:26:33 -0300

    upd: main style & config files

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,11 +5,42 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'pocket': {'min': '280px', 'max': '479px'},
+      'phone': {'min': '480px', 'max': '639px'},
+      'tablet': {'min': '640px', 'max': '767px'},
+      'laptop': {'min': '768px', 'max': '1023px'},
+      'desktop': {'min': '1024px', 'max': '1279px'},
+      'ultrawide': { 'min': '1280px', 'max': '9999px'}
+    },
+    fontFamily: {
+      poppins: ['poppins, "sans-serif"']
+    },
     extend: {
       spacing: {
         '110': '36rem',
-      }
-    }
+      },
+      animation: {
+        text: "text 5s ease infinite",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+      colors: {
+        "black-rgba": "rgba(0, 0, 0, 0.5)",
+        "transparent": "rgba(255, 255, 255, 0)",
+        "glass-white": "rgba(255, 255, 255, 0.1)",
+      },
+    },
   },
   plugins: [],
 }
