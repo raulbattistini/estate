985ddaf955ead1ea6230ca639320ea0ae880320a
Raul Battistini <raulbattistini.3@gmail.com>
2022-10-17 16:08:44 -0300

    add: new route and requireauth

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Blog } from "./pages/Blog";
@@ -11,24 +12,31 @@ import { Rent } from "./pages/Rent";
 import { RegisterUserSec } from "./pages/RegisterUser/SecStep";
 import { ForgotPassword } from "./pages/ForgotPassword";
 import { Welcome } from "./pages/Welcome";
-import { RequireAuth } from "./contexts/Auth/RequireAuth";
+import { store } from "./app/store";
+import { AvailableProperties } from "./pages/AvailableProperties";
+import { RequireAuth } from "./feats/Auth/RequireAuth";
 
 export const RoutesList = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register-user" element={<RegisterUser />} />
-        <Route path="/register-user-2" element={<RegisterUserSec />} />
-        <Route path="/welcome" element={<RequireAuth><Welcome /></RequireAuth>} />
-        <Route path="/register-property" element={<RegisterProperty />} />
-        <Route path="/buy" element={<Buy />} />
-        <Route path="/sell" element={<Sell />} />
-        <Route path="/rent" element={<Rent />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="*" element={<NotFound />} />
+        <Provider store={store}>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register-user" element={<RegisterUser />} />
+          <Route path="/register-user-2" element={<RegisterUserSec />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route element={<RequireAuth />}>
+            <Route path="/available-properties" element={<AvailableProperties />} />
+            <Route path="/register-property" element={<RegisterProperty />} />
+            <Route path="/buy" element={<Buy />} />
+            <Route path="/sell" element={<Sell />} />
+            <Route path="/rent" element={<Rent />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Provider>
       </Routes>
     </Router>
   );
