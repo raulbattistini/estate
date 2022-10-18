3186e73c710f9ccf8e4234f29cb032eed463ceb7
Raul Battistini <raulbattistini.3@gmail.com>
2022-10-18 16:37:17 -0300

    upd: fitting to new auth

--- web/src/pages/AvailableProperties/index.tsx
+++ web/src/pages/AvailableProperties/index.tsx
@@ -1,9 +1,8 @@
 import { CSSProperties, useState } from "react";
-import { useGetPropertiesQuery } from "../../services/apiPropertiesSlice";
 import ClipLoader from "react-spinners/ClipLoader";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
-import { IProperty } from "../../interfaces";
+
 
 export const AvailableProperties = () => {
   let [color, setColor] = useState("#ffffff");
@@ -14,31 +13,20 @@ export const AvailableProperties = () => {
     borderColor: "red",
   };
 
-  const { data: properties, isLoading, isError, error, isSuccess } = useGetPropertiesQuery("");
-
   let content: JSX.Element | null;
-  if (isLoading) {
-    content = <ClipLoader color={color} cssOverride={override} size={150} />;
-  } else if (isSuccess) {
     content = (
       <div className="w-full">
         <Header />
         <div className="bg-[#21a0a0]">
           Properties available:
           <ul>
-            {properties.map((property: IProperty, i: number) => {
-              return <li key={property.estate_id}> {property.name} </li>;
-            })}
+          dfdfdfd
           </ul>
         </div>
         <Footer />
       </div>
     );
-  } else if (isError){
-    content = <p>{JSON.stringify(error)}</p>
-  } else {
-    return <>Unexpected error.</>
-  }
+ 
 
   return content;
 };
--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,133 +1,118 @@
 import { Formik, Form } from "formik";
-import { useState, useEffect, useRef, CSSProperties } from "react";
-import { useDispatch } from "react-redux";
+import { useState, useEffect, useRef, CSSProperties, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import ClipLoader from "react-spinners/ClipLoader";
 import { toast } from "react-toastify";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
-import { useLoginMutation } from "../../feats/Auth/authApiSlice";
+import { AuthContext } from "../../contexts/Auth/AuthContext";
+import { loginSchema } from "../../helpers/schema";
 
 export const Login = () => {
   // usestate
   let [color, setColor] = useState("#ffffff");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-
   // useeffect
   useEffect(() => {
     emailRef!.current!.focus();
   }, []);
 
-  useEffect(() => {
-    setErrorMessage("");
-  }, [email, password]);
-
   // useref
-  const errorRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement | null>(null);
 
   // lib hooks
+  const auth = useContext(AuthContext);
   const navigate = useNavigate();
-  const [login, { isLoading }] = useLoginMutation();
-  const dispatch = useDispatch();
 
-  // functions
-  const handleSubmit = async (e: HTMLFormElement) => {
-    e.preventDefault();
-
+  const handleLogin = async (email: string, password: string) => {
     try {
-    } catch (error: any) {
-      if (!error?.originalStatus) {
-        setErrorMessage("No server response");
-        toast.error(`Something wrong occurred: ${error.message} || ${errorMessage}`);
-      } else if (error.originalStatus === 400) {
-        setErrorMessage("Fill in the whole body");
-        toast.error(`Something wrong occurred: ${error.message} || ${errorMessage}`);
-      } else if (error.originalStatus === 401) {
-        setErrorMessage("Unauthorized");
-        toast.error(`Something wrong occurred: ${error.message} || ${errorMessage}`);
-      } else {
-        setErrorMessage("Login failed");
-        toast.error(`Something wrong occurred: ${error.message} || ${errorMessage}`);
+      if (email && password) {
+        const isLogged = await auth.signin(email, password);
+        console.log(isLogged);
+        if (isLogged) {
+          toast.success(`Welcome ${auth!.user}`);
+          navigate("/available-properties");
+        } else {
+          navigate("/login");
+        }
       }
+    } catch (error: any) {
+      toast.error(error.message);
     }
   };
-
-  const handleChangeEmail = (e: React.BaseSyntheticEvent) => {
-    setEmail(e.currentTarget.value);
-  };
-  const handleChangePassword = (e: React.BaseSyntheticEvent) => {
-    setPassword(e.currentTarget.value);
-  };
-
   const override: CSSProperties = {
     display: "block",
     margin: "0 auto",
     borderColor: "red",
   };
-
-  const content = isLoading ? (
-    <ClipLoader color={color} cssOverride={override} size={150} />
-  ) : (
+  let content;
+  content = (
     <>
       <div className="w-full">
         <Header />
-        <p 
-        ref={errorRef} 
-        className={errorMessage ? "errmsg" : "offscreen"} 
-        aria-live="assertive"
-        >
-          {errorMessage}
-        </p>
         <div className="bg-[#21a0a0] flex flex-col text-center pb-5">
           <h1 className="text-2xl text-white pt-3">Login to see your account</h1>
 
           <>
-            <form
-              onSubmit={() => {
-                handleSubmit;
-              }}
+            <Formik
+            initialValues={{
+              email: "",
+              password: ""
+            }}
+            onSubmit={(values)=>{handleLogin(values.email, values.password)}}
+            validationSchema={loginSchema}
             >
-              <label className="pt-3 text-white justify-center flex self-center">Email</label>
-              <input
-                ref={emailRef}
-                className="outline-0 w-1/5 pl-2 p-1 rounded-sm"
-                type="email"
-                placeholder="Insert your email..."
-                value={email}
-                onChange={handleChangeEmail}
-                name="email"
-              />
-              <label className="pt-3 text-white justify-center flex self-center">Password</label>
-              <input
-                className="outline-0 w-1/5 pl-2 p-1 rounded-sm"
-                type="password"
-                placeholder="Insert your password..."
-                value={password}
-                onChange={handleChangePassword}
-                name="password"
-              />
-
-              <span className="flex flex-col text-white pt-3">
-                <Link to="/forgot-password" className="text-center text-purple-200">
-                  Forgot your password?
-                </Link>
-              </span>
-              <span className="flex flex-col text-white pt-3">
-                Don't have an account?
-                <Link to="/login" className="text-center text-green-200">
-                  Register now
-                </Link>
-              </span>
-              <button
-                type="submit"
-                className="p-3 mt-5 pl-5 pr-5 rounded-md bg-[#048865] text-white hover:bg-green-500"
-              >
-                Login
-              </button>
-            </form>{" "}
+              {({ values, errors, handleChange, touched }) => {
+                return (
+                  <Form>
+                    <label className="pt-3 text-white justify-center flex self-center">Email</label>
+                    <input
+                      ref={emailRef}
+                      className="outline-0 w-1/5 pl-2 p-1 rounded-sm"
+                      type="text"
+                      placeholder="Insert your email..."
+                      value={values.email}
+                      onChange={handleChange}
+                      name="email"
+                    />
+                    <span className="text-red-300 mt-2 text-center flex flex-col">
+                        {!!touched.email && errors.email}
+                        {touched.email && !!errors.email}
+                      </span>
+                    <label className="pt-3 text-white justify-center flex self-center">Password</label>
+                    <input
+                      className="outline-0 w-1/5 pl-2 p-1 rounded-sm"
+                      type="password"
+                      placeholder="Insert your password..."
+                      value={values.password}
+                      onChange={handleChange}
+                      name="password"
+                      required
+                    />
+                    <span className="text-red-300 mt-2 text-center flex flex-col">
+                        {!!touched.password && errors.password}
+                        {touched.password && !!errors.password}
+                      </span>
+                    <span className="flex flex-col text-white pt-3">
+                      <Link to="/forgot-password" className="text-center text-purple-200">
+                        Forgot your password?
+                      </Link>
+                    </span>
+                    <span className="flex flex-col text-white pt-3">
+                      Don't have an account?
+                      <Link to="/login" className="text-center text-green-200">
+                        Register now
+                      </Link>
+                    </span>
+                    <button
+                      type="submit"
+                      className="p-3 mt-5 pl-5 pr-5 rounded-md bg-[#048865] text-white hover:bg-green-500"
+                    >
+                      Login
+                    </button>
+                  </Form>
+                );
+              }}
+            </Formik>{" "}
           </>
         </div>
 
