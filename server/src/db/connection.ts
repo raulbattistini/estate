e90b8772be3bed7fcd4612f844caec57b0af70de
Raul Battistini <raulbattistini.3@gmail.com>
2022-10-20 16:25:22 -0300

    upd: db connection

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,14 +1,18 @@
 import { DataSource } from "typeorm";
-import 'dotenv/config'
+import "dotenv/config";
 import { User } from "../models/User";
+import { Property } from "../models/Property";
+import { Image } from "../models/Image";
+import { Mail } from "../models/Mail";
 
 export const connection = new DataSource({
-    type: 'postgres',
-    database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT as unknown as number,
-    entities: [User],
-    migrations: ['./migrations/']
-})
+  type: "postgres",
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT as unknown as number,
+  entities: [User, Property, Image, Mail],
+  migrations: ["./db/migrations/"],
+  synchronize: true,
+});
