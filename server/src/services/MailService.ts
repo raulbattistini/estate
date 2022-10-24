93cf87a8872c3fa2f63e84f850c0176fa2978a25
Raul Battistini <raulbattistini.3@gmail.com>
2022-10-24 16:54:09 -0300

    upd: mail service

--- server/src/services/MailService.ts
+++ server/src/services/MailService.ts
@@ -1,39 +1,48 @@
-import fs from 'fs';
-import nodemailer, { Transporter } from 'nodemailer';
-import handlebars from 'handlebars';
+import fs from "fs";
+import nodemailer, { Transporter } from "nodemailer";
+import handlebars from "handlebars";
 import { ISendService } from "../interfaces";
 
-
 export class MailService {
-   private client: Transporter;
+  private client: Transporter;
 
-   async sendMail ({to, subject, variables, path}: ISendService){
-      await nodemailer.createTestAccount().then((testAccount) =>{
-         const transporter = nodemailer.createTransport({
-            host: testAccount.smtp.host,
-            port: testAccount.smtp.port,
-            secure: testAccount.smtp.secure,
-            auth: {
-               user: testAccount.user,
-               pass: testAccount.pass
-            }
-         })
-         this.client = transporter;
-       });
-      const templateFileContent = fs.readFileSync(path).toString('utf-8');
-
-      const mailTemplateParse = handlebars.compile(templateFileContent);
-
-      const html = mailTemplateParse(variables);
-
-      const message = await this.client.sendMail({
-         to,
-         subject,
-         html,
-         from: "FLRE <noreply@fl-re.com>"
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: { 
+      id: string; 
+      name: string; 
+      title: string; 
+      description: string 
+   },
+    path: string
+  ) {
+    await nodemailer.createTestAccount().then((testAccount) => {
+      const transporter = nodemailer.createTransport({
+        host: testAccount.smtp.host,
+        port: testAccount.smtp.port,
+        secure: testAccount.smtp.secure,
+        auth: {
+          user: testAccount.user,
+          pass: testAccount.pass,
+        },
       });
+      this.client = transporter;
+    });
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
 
-      console.log("Message sent: %s", message.id);
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(message));
-   }
+    const mailTemplateParse = handlebars.compile(templateFileContent);
+
+    const html = mailTemplateParse(variables);
+
+    const message = await this.client.sendMail({
+      to,
+      subject,
+      html,
+      from: "FLRE <noreply@fl-re.com>",
+    });
+
+    console.log("Message sent: %s", message.id);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(message));
+  }
 }
