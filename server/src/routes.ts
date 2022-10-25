bbc84cebbf1bc4bdbf8dcd71a04cb9b69651c0f1
Raul Battistini <raulbattistini.3@gmail.com>
2022-10-25 15:57:36 -0300

    add: routes

---
+++ server/src/routes.ts
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import multer from "multer";
+import { ImageController } from "./controllers/ImageController";
+import { MailController } from "./controllers/MailController";
+import { PostController } from "./controllers/PostController";
+import { UserController } from "./controllers/UserController";
+import { uploadMiddleware } from "./middlewares/uploadMiddleware";
+
+export const routes = Router();
+
+const postController = new PostController();
+
+const userController = new UserController();
+
+const imageController = new ImageController()
+
+// const mailController = new MailController();
+
+// routes.post("/api/mails", mailController.sendMail);
+
+routes.get("/api/posts", postController.get);
+
+routes.get("/api/posts/:id", postController.getById);
+
+routes.post("/api/posts", postController.post);
+
+routes.put("/api/posts", postController.update);
+
+routes.delete("/api/posts/:id", postController.deleteById);
+
+routes.post("/api/users", userController.create);
+
+routes.get("/api/users/", userController.listUsers);
+
+routes.post("/api/tokens", userController.authenticate);
+
+routes.get("/api/images", imageController.read);
+
+routes.post("/api/images", multer(uploadMiddleware).single("image"), imageController.upload);
+
+routes.delete("/api/images/:id", imageController.delete);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from 'cors';
 import "dotenv/config";
 import { connection } from "./db/connection";
+import { routes } from "./routes";
 
 const app = express();
 
@@ -10,12 +12,15 @@ connection
   .initialize()
   .then(() => {
     console.log("Postgres database initialized");
+    app.use(express.json());
+    app.use(cors())
+    app.use(routes);
+
   })
   .catch((error: any) => {
     console.log("Something wrong occurred: ", error.message);
   });
 
-  
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
