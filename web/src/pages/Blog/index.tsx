9a1157a6f587b8c8246e8c10a426cb020aa096f0
Raul Battistini <raulbattistini.3@gmail.com>
2022-10-26 16:49:36 -0300

    upd: unfinished daily changes

--- web/src/pages/Blog/index.tsx
+++ web/src/pages/Blog/index.tsx
@@ -1,44 +1,84 @@
+import { useState, useEffect } from "react";
 import { Grid } from "@mui/material";
+import moment from "moment";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 import defaultImg from "../../assets/images/default.jpg";
+import { api } from "../../services/api";
+import { IPost } from "../../interfaces";
 
 export const Blog = () => {
+
+  const m = moment().toDate();
+
+  const [postData, setPostData] = useState<IPost[]>([
+    {
+      post_id: "",
+      title: "",
+      content: "",
+      created_at: m,
+    },
+  ]);
+
+  useEffect(() => {
+    loadPost();
+    console.log(postData);
+  }, [postData]);
+
+  const load = async () => {
+    const res = await api.get("/posts");
+    console.log(res.data);
+    setPostData(res.data);
+  };
+
+  const loadPost = async () => {
+    try {
+      const load = async () => {
+        const res = await api.get("/posts");
+        setPostData(res.data);
+      };
+      load();
+    } catch (error: any) {
+      // throw new Error(`Something wrong occurred: ${error.message}`);
+      console.error(`Something wrong occurred: ${error.message}`);
+    }
+  };
+
+  const getSinglePost = async (id: string) => {
+    const res = await api.get(`/posts/${id}`);
+  };
+
   return (
     <div className="w-full">
       <Header />
       <div className="bg-[#21a0a0]">
-        <h1 className="text-3xl text-white text-center pt-4">
-          Keep yourself up to date with our posts!
-        </h1>
+        <h1 className="text-3xl text-white text-center pt-4">Keep yourself up to date with our posts!</h1>
         <div className="flex justify-center">
           <article className="max-w-lg content-center flex justify-center flex-col items-center pt-3">
             <h2 className="z-10 text-white text-center text-lg font-extralight pb-3 justify-center flex flex-col">
-            Here we talk about how to buy your first property, investing with real estate, talks with masters from the area such as Grant Cardone, legal assesments we get from experienced professional lawyers and much more!
+              Here we talk about how to buy your first property, investing with real estate, talks with masters from the
+              area such as Grant Cardone, legal assesments we get from experienced professional lawyers and much more!
             </h2>
           </article>
         </div>
         <Grid container className="place-items-center text-center">
           <Grid item xs={5}>
             <div className="flex justify-center flex-col self-center pt-3 pb-5">
-              <img
-                src={defaultImg}
-                alt="postImage"
-                className="rounded-md justify-self-center w-56 h-32 self-center"
-              />
+              <img src={defaultImg} alt="postImage" className="rounded-md justify-self-center w-56 h-32 self-center" />
             </div>
           </Grid>
           <Grid item xs={7}>
             <div className="flex flex-col text-white">
-              <title className="flex flex-col text-2xl">
-                Blog title from api map
-              </title>
-              <span className="mr-9">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
-                quam error aut corrupti, nisi tempora tempore eligendi dolorem
-                saepe rem aspernatur eos labore aliquam. Error mollitia placeat
-                consequuntur consectetur consequatur.
-              </span>
+              {postData?.map((posts: IPost, key) => {
+                return (
+                  <>
+                    <title className="flex flex-col text-2xl" key={posts.post_id}>
+                      {posts.title}
+                    </title>
+                    <span className="mr-9">{posts.content}</span>
+                  </>
+                );
+              })}
             </div>
           </Grid>
         </Grid>
--- web/src/pages/Welcome/index.tsx
+++ web/src/pages/Welcome/index.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
+import { AuthContext } from "../../contexts/Auth/AuthContext";
 
 export const Welcome = () => {
+  
+  const auth = useContext(AuthContext);
+
+  const string = JSON.stringify((auth.user));
+
+  useEffect(()=>{
+    console.log(auth.user, string)
+  }, [])
+
    return (
     <div className="w-full">
       <Header />
       <div className="bg-[#21a0a0]">
-      
+        <p className="text-white">Olá {auth.user?.name} {string} </p>    
         <Link to="/available-properties"> See available properties </Link>
       </div>
       <Footer />
