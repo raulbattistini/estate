5950f266ee6041991694b5769f9dbe6e82773412
Raul Battistini <raulbattistini.3@gmail.com>
2022-10-27 16:29:43 -0300

    upd: minor logic to gather user info from api (will be refactored)

--- web/src/pages/Welcome/index.tsx
+++ web/src/pages/Welcome/index.tsx
@@ -1,25 +1,44 @@
-import React, { useContext, useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { AuthContext } from "../../contexts/Auth/AuthContext";
+import { IUserAuth } from "../../interfaces";
 
 export const Welcome = () => {
-  
   const auth = useContext(AuthContext);
 
-  const string = JSON.stringify((auth.user));
+  const string = JSON.stringify(auth.user?.name);
 
-  useEffect(()=>{
-    console.log(auth.user, string)
-  }, [])
+  useEffect(() => {
+    console.log(auth.user, string);
+  }, []);
+  
+  const regex = /(<([^>]+)>)/gi;
 
-   return (
+  return (
     <div className="w-full">
       <Header />
       <div className="bg-[#21a0a0]">
-        <p className="text-white">Olá {auth.user?.name} {string} </p>    
-        <Link to="/available-properties"> See available properties </Link>
+        {auth.user !== null ? (
+          <div className="text-white">
+            {auth!.user!.map((userInfo: IUserAuth, key: number) => {
+              return (
+                <div>
+                  <span className="flex justify-center text-xl pt-5"> Hi {userInfo.name}, how you doing today?</span>
+                  <span className="flex justify-center text-xl pt-5"> Do you want to {userInfo.intention!.replace(regex, "")}?</span>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          ""
+        )}
+        <div className="flex justify-center">
+          <span className="flex text-white self-center">
+            <Link to="/available-properties"> Or maybe... See available properties </Link>
+          </span>
+        </div>
       </div>
       <Footer />
     </div>
