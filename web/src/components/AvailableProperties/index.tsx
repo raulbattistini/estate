c4776e84f0d375d1d0a1c50fb5b882dd56a554c7
Raul Battistini <raulbattistini.3@gmail.com>
2022-10-30 05:15:16 -0300

    upd: managing folders

---
+++ web/src/components/AvailableProperties/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../Header";
+import { Footer } from "../Footer";
+
+export const AvailableProperties = () => {
+  return (
+    <div className="w-full">
+    
+    </div>
+  );
+};
