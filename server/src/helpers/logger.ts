f2a8c90d0cec29ad687d8bc00319d1c581034335
Raul Battistini <raulbattistini.3@gmail.com>
2022-12-23 12:02:28 -0300

    upd: logger

--- server/src/helpers/logger.ts
+++ server/src/helpers/logger.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import * as winston from "winston";
+import ip from 'ip'
 import { format } from "winston";
 
 const fileDir = "C:/workarea/byme/estate/server/src/logs/";
@@ -8,7 +9,7 @@ const fileName = "C:/workarea/byme/estate/server/src/logs/log.log";
 const logFormatter = format.printf((info) => {
   let { timestamp, level, stack, message } = info;
   message = stack || message;
-  return `${timestamp} ${level}: ${message}`;
+  return `${timestamp} from ${ip.address()} ${level}: ${message}`;
 });
 
 const transportDoc = new winston.transports.File({
